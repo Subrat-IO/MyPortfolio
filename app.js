63ca696ad1b7e391099359af1520d451c66af7d5
Subrat <subratsethi501@gmail.com>
2025-05-29 02:13:37 +0530

    update

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const video1 = document.getElementById('projectvid-1');
 const video2 = document.getElementById('projectvid-2');
 const video3 = document.getElementById('projectvid-3');
+const hoverSign = document.querySelector(".hoverSign");
 
 const vidlist = [video1,video2,video3];
 
 vidlist.forEach(function(video){
     video.addEventListener('mouseover',function(){
         video.play()
+        hoverSign.classList.add("active")
     })
     video.addEventListener('mouseout',function(){
         video.pause()
+        hoverSign.classList.remove("active")
     })
 })
+
+
