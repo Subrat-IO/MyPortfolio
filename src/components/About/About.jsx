0d7680ea0468ed74737499cf11586d26ad9af44f
subrat sethi <subratsethi501@gmail.com>
2025-11-02 22:21:51 +0530

    Resume Update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -113,7 +113,7 @@ const About = () => {
           </motion.p>
 
           <motion.a
-            href="https://drive.google.com/file/d/1izyvJ3mdgd7tDHyy0XcOa9K1lfcU-Y5r/view?usp=sharing"
+            href="https://drive.google.com/file/d/1X7e0nsK870qxq70JrIsEkGh9HWRqEc-o/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
